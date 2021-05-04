c3502e7391ceb8b05dc148ff0090e23e9358bd6e
Szymon <s17655@pjwstk.edu.pl>
2021-05-04 02:14:31 +0200

    Added MainView

--- app/src/main/java/pl/edu/pja/financemanager/MainActivity.kt
+++ app/src/main/java/pl/edu/pja/financemanager/MainActivity.kt
@@ -2,10 +2,12 @@ package pl.edu.pja.financemanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pl.edu.pja.financemanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val mainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainBinding.root)
     }
 }
